a19319b303c6d34e7d1fe3dc9876b72bdc9a78ee
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 07:38:16 -0700

    Agregar archivos de proyecto.

---
+++ PracticaConstructores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaConstructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+
+            Console.WriteLine("Alumno 1: ");
+            Console.WriteLine("Nombre: " + alumno1.Nombre);
+            Console.WriteLine("Matricula: " + alumno1.Matricula);
+
+            //Usar el constructor sin parametros
+            Alumno alumno2 = new Alumno();
+
+            //Despues establece sus atributos
+            alumno2.Nombre = "Pedro";
+            alumno2.Matricula = "Abc123";
+
+            Console.WriteLine("Alumno 2: ");
+            Console.WriteLine("Nombre: " + alumno2.Nombre);
+            Console.WriteLine("Matricula: " + alumno2.Matricula);
+
+            Alumno alumno3 = new Alumno("Ana","XYZ987");
+            Console.WriteLine("Alumno 3: ");
+            Console.WriteLine("Nombre: " + alumno3.Nombre);
+            Console.WriteLine("Matricula: " + alumno3.Matricula);
+
+            Console.Read();
+        }
+    }
+}
